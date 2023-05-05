2394ef8c8a06ac7d7522d0fe8192dd43d3cfd46a
Anna Esteve <aesteve8@gmail.com>
2023-05-05 17:02:31 +0200

    nav started to style

--- src/components/Navigation/NavComp.vue
+++ src/components/Navigation/NavComp.vue
@@ -1,12 +1,15 @@
 <template>
-  <header>
+  <!-- <header>
     <nav>
+
       <div class="nav-border-bottom">
         <router-link to="/">
-          <img src="../../img/a_faire.png" alt="logo" class="logo" />
+          <img src="@/img/a_faire.png" alt="logo" class="logo" />
         </router-link>
       </div>
+
       <div id="nav-main">
+        <p class="hide">Welcome, {{ user.email }}!</p>
         <div id="mainNavigation">
           <router-link to="/" class="active">
             <fa icon="fa-circle-dot" />
@@ -15,30 +18,76 @@
           <button @click="handleSignOut">Log out</button>
         </div>
       </div>
+
     </nav>
+  </header> -->
+  <header>
+    <div>
+      <router-link to="/">
+        <!-- <img src="@/img/logo.png" alt="logo" class="logo smallLogo" />
+        <img src="@/img/a_faire.png" alt="logo" class="logo hide" /> -->
+        <div>Welcome, {{ user.email }}</div>
+      </router-link>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <router-link to="/" class="active">
+            <fa icon="fa-solid fa-code" /> <span class="hide">Tasks</span>
+          </router-link>
+        </li>
+        <li>
+          <a href="#">
+            <fa icon="fa-solid fa-font-awesome" /> <span class="hide">Flagged tasks</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <fa icon="fa-regular fa-bell" /> <span class="hide">Notifications</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <fa icon="fa-solid fa-chart-line" /> <span class="hide">Analytics</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <div>
+      <ul>
+        <li>
+          <a @click="handleSignOut">
+            <fa icon="fa-solid fa-code" /> <span class="hide">Log out</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <fa icon="fa-solid fa-gear" /> <span class="hide">Settings</span>
+          </a>
+        </li>
+      </ul>
+    </div>
   </header>
 </template>
 
 <script>
-import { mapActions } from 'pinia';
-import UserStore from '@/stores/user';
-
-import UserLogo from '../Account/UserLogoComp.vue';
+import { mapState, mapActions } from 'pinia'
+import UserStore from '@/stores/user'
 
 export default {
   name: 'HeaderComp',
-  components: {
-    UserLogo,
+  computed: {
+    ...mapState(UserStore, ['user'])
   },
   methods: {
     ...mapActions(UserStore, ['signOut']),
 
-    async handleSignOut(){
-      try{
-        await this.signOut();
+    async handleSignOut() {
+      try {
+        await this.signOut()
         this.$router.push({ name: 'sign-in' })
-      } catch(e){
-        console.error(e);
+      } catch (e) {
+        console.error(e)
       }
     }
   }
@@ -48,9 +97,9 @@ export default {
 <style scoped>
 header {
   box-sizing: border-box;
-  padding: 30px 10px;
+  padding: 25px;
 
-  background-color: #ebeded;
+  background-color: blue;
 
   height: 100vh;
   width: 90px;
@@ -58,8 +107,40 @@ header {
   position: fixed;
   top: 0;
   left: 0;
+
+  transition: all 0.3s ease;
 }
 
+/** HOVER **/
+header:hover {
+  background-color: red;
+  width: 320px;
+}
+header:hover .hide {
+  display: block;
+}
+header:hover .logo.smallLogo{
+  display: none;
+}
+/*************/
+.hide {
+  display: none;
+}
+header .logo{
+  width: 100%;
+}
+header .logo.hide{
+  width: 30%;
+}
+
+header nav{
+  margin-top: 30px;
+}
+
+header ul{
+  list-style-type: none;
+}
+/* 
 header nav {
   height: 100%;
 
@@ -67,15 +148,6 @@ header nav {
   flex-flow: column nowrap;
   justify-content: space-between;
   align-items: center;
-}
-
-header:hover{
-  background-color: red;
-  width: 320px;
-}
-
-header nav > div {
-  padding-bottom: 10px;
 }
 
 header svg {
@@ -122,5 +194,5 @@ header nav #nav-main #mainNavigation {
 
   color: var(--tertiary-accent);
   font-weight: 700;
-}
+} */
 </style>
