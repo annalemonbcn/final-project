98b9f2741502e389eeda5f89c4ccc2735611fd28
Anna Esteve <aesteve8@gmail.com>
2023-05-16 12:24:49 +0200

    new task changed for mobile

---
+++ src/components/Workspace/AddTaskMobileComp.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="addTask">
+    <router-link to="/task/add-task">
+      <fa icon="fa-solid fa-plus" />
+    </router-link>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+#addTask{
+  background-color: var(--green-accent);
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  display: flex;
+  align-items: center;
+  position: absolute;
+  top: auto;
+  left: auto;
+  bottom: 50px;
+  right: 50px;
+}
+
+#addTask a{
+  width: 100%;
+  display: block;
+  text-align: center;
+  color: white;
+}
+</style>
